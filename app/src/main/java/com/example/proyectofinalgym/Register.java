eeaa1d9c5c5e54d8b164725422600fbd8c5dda1c
unknown <bemimarket@gmail.com>
2024-02-27 12:59:43 +0100

    comprobacion dni

--- app/src/main/java/com/example/proyectofinalgym/Register.java
+++ app/src/main/java/com/example/proyectofinalgym/Register.java
@@ -12,9 +12,12 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Register extends AppCompatActivity {
 
-    private EditText editTextNombre,editTextEdad,editTextDni,editTextTelefono,editTextEmail,editTextPassword,editTextPeso,editTextAltura;
+    private EditText editTextNombre, editTextEdad, editTextDni, editTextTelefono, editTextEmail, editTextPassword, editTextPeso, editTextAltura;
 
     private CheckBox checkBoxPolitica;
     private Button buttonResgitrar;
@@ -40,28 +43,34 @@ public class Register extends AppCompatActivity {
         buttonResgitrar = findViewById(R.id.buttonResgitrar);
 
 
-
         buttonResgitrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!editTextDni.getText().toString().equals("") && !editTextEmail.getText().toString().equals("") && !editTextPassword.getText().toString().equals("")){
+                if (!editTextDni.getText().toString().equals("") && !editTextEmail.getText().toString().equals("") && !editTextPassword.getText().toString().equals("")) {
 
-                    if (checkBoxPolitica.isChecked()){
-                        User user = new User(editTextEmail.getText().toString(),editTextPassword.getText().toString()
-                                ,editTextDni.getText().toString(),editTextNombre.getText().toString(),editTextPeso.getText().toString(),
-                                editTextAltura.getText().toString(),editTextTelefono.getText().toString(),editTextEdad.getText().toString());
+                    if (checkBoxPolitica.isChecked()) {
+                        if (comprobarDni(editTextDni.getText().toString())) {
+                            User user = new User(editTextEmail.getText().toString(), editTextPassword.getText().toString()
+                                    , editTextDni.getText().toString(), editTextNombre.getText().toString(), editTextPeso.getText().toString(),
+                                    editTextAltura.getText().toString(), editTextTelefono.getText().toString(), editTextEdad.getText().toString());
 
-                        dbHandler.addNewUser(user);
-                        dbHandler.close();
-                        Intent paginaResgistro = new Intent(Register.this, LoginActivity.class);
-                        paginaResgistro.putExtra("email",editTextEmail.getText());
-                        paginaResgistro.putExtra("password",editTextPassword.getText());
-                        startActivity(paginaResgistro);
-                    }else {
-                        Toast.makeText(Register.this,"Debes aceptar la politica de privacidad",Toast.LENGTH_LONG).show();
+
+                            dbHandler.addNewUser(user);
+                            dbHandler.close();
+                            Intent paginaResgistro = new Intent(Register.this, LoginActivity.class);
+                            paginaResgistro.putExtra("email", editTextEmail.getText());
+                            paginaResgistro.putExtra("password", editTextPassword.getText());
+                            startActivity(paginaResgistro);
+                        } else {
+                            Toast.makeText(Register.this, "El DNI introducido no existe", Toast.LENGTH_LONG).show();
+
+                        }
+
+                    } else {
+                        Toast.makeText(Register.this, "Debes aceptar la politica de privacidad", Toast.LENGTH_LONG).show();
                     }
-                }else {
-                    Toast.makeText(Register.this,"Faltan campos obligatorios",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Register.this, "Faltan campos obligatorios", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -156,4 +165,51 @@ public class Register extends AppCompatActivity {
     public void setDbHandler(DBHandler dbHandler) {
         this.dbHandler = dbHandler;
     }
+
+    public boolean comprobarDni(String dni) {
+        if (dni.length() != 9) {
+            return false;
+        }
+        Map<Integer, String> map = crearMapaDNI();
+        String letra = String.valueOf(dni.charAt(dni.length()-1));
+        try {
+            int n = Integer.parseInt(dni.substring(0, dni.length()-1));
+            int num = n%23;
+            return map.get(num).equalsIgnoreCase(letra);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Map<Integer, String> crearMapaDNI() {
+        Map<Integer, String> dniMap = new HashMap<>();
+
+        // Asignar números del 0 al 22 con los caracteres correspondientes
+        dniMap.put(0, "T");
+        dniMap.put(1, "R");
+        dniMap.put(2, "W");
+        dniMap.put(3, "A");
+        dniMap.put(4, "G");
+        dniMap.put(5, "M");
+        dniMap.put(6, "Y");
+        dniMap.put(7, "F");
+        dniMap.put(8, "P");
+        dniMap.put(9, "D");
+        dniMap.put(10, "X");
+        dniMap.put(11, "B");
+        dniMap.put(12, "N");
+        dniMap.put(13, "J");
+        dniMap.put(14, "Z");
+        dniMap.put(15, "S");
+        dniMap.put(16, "Q");
+        dniMap.put(17, "V");
+        dniMap.put(18, "H");
+        dniMap.put(19, "L");
+        dniMap.put(20, "C");
+        dniMap.put(21, "K");
+        dniMap.put(22, "E");
+
+        return dniMap;
+    }
+
 }
